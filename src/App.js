a82ae8071cdb4f279c1db1889768809bb6ff5eb3
fiqrioemry <foemry@gmail.com>
2024-10-22 22:16:02 +0700

    add section leftbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </Router>
+    </main>
+  );
 }
 
 export default App;
--- src/component/LeftBar.jsx
+++ src/component/LeftBar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { FaHome } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const LeftBar = () => {
-  return <div>LeftBar</div>;
+  return (
+    <section>
+      <div className="fixed top-0 bottom-0 left-0 w-[350px]">
+        <Link to="/">
+          <div>
+            <FaHome />
+          </div>
+          <div>Home</div>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default LeftBar;
