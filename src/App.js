4e143e616a5f1311fc125514dd6dc2fbf018ca13
fiqrioemry <foemry@gmail.com>
2024-10-22 22:20:08 +0700

    adding right bar section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
+import LeftBar from "./component/LeftBar";
+import RightBar from "./component/RightBar";
 
 function App() {
   return (
     <main>
       <Router>
+        <LeftBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/profile" element={<ProfilePage />} />
         </Routes>
+        <RightBar />
       </Router>
     </main>
   );
--- src/component/LeftBar.jsx
+++ src/component/LeftBar.jsx
@@ -2,15 +2,39 @@ import React from "react";
 import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
 const LeftBar = () => {
+  const navmenu = [
+    {
+      icon: <FaHome />,
+      title: "Home",
+    },
+    {
+      icon: <FaHome />,
+      title: "Home",
+    },
+    {
+      icon: <FaHome />,
+      title: "Home",
+    },
+    {
+      icon: <FaHome />,
+      title: "Home",
+    },
+    {
+      icon: <FaHome />,
+      title: "Home",
+    },
+  ];
   return (
     <section>
-      <div className="fixed top-0 bottom-0 left-0 w-[350px]">
-        <Link to="/">
-          <div>
-            <FaHome />
-          </div>
-          <div>Home</div>
-        </Link>
+      <div className="fixed top-0 bottom-0 left-0 w-[350px] bg-blue-500">
+        <div className="">
+          <Link to="/">
+            <div>
+              <FaHome />
+            </div>
+            <div>Home</div>
+          </Link>
+        </div>
       </div>
     </section>
   );
