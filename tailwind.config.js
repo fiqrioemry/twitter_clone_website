70c5553dd0f4a2d426fbd17a3c84569f08f5b92d
fiqrioemry <foemry@gmail.com>
2024-10-22 22:11:34 +0700

    setting route for pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      DEFAULT: "Poppins",
+      primary: "Poppins",
+      secondary: "Manrope",
+      tertiary: "Montserrat",
+    },
+    screens: {
+      sm: "576px",
+      md: "850px",
+      lg: "1000px",
+      xl: "1200px",
+    },
+    extend: {
+      colors: {
+        primary: "#FFFFFF",
+        secondary: "#000000",
+        tertiary: "#DB4444",
+      },
+    },
   },
   plugins: [],
-}
-
+};
